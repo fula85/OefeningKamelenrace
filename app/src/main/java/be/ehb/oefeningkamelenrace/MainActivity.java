f71dcebbafd3ee66624e73257c796f851422b060
fula85 <carine_fulakula@hotmail.com>
2019-03-12 11:09:37 +0100

    handlers added

--- app/src/main/java/be/ehb/oefeningkamelenrace/MainActivity.java
+++ app/src/main/java/be/ehb/oefeningkamelenrace/MainActivity.java
@@ -1,14 +1,19 @@
 package be.ehb.oefeningkamelenrace;
 
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import be.ehb.oefeningkamelenrace.util.LoadingHandler;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView tvLoadingEen;
     private ProgressBar pbLoading;
+    private LoadingHandler loadingHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,33 @@ public class MainActivity extends AppCompatActivity {
 
         pbLoading = findViewById(R.id.pb_loading);
         tvLoadingEen = findViewById(R.id.tv_lbl_een);
+    }
+
+    public void onStartClicked(View view) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <=20; i++){
+                    Message message = new Message();
+                    message.arg1 = i;
+                    loadingHandler.sendMessage(message);
+                }
+            }
+        });
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
     }
 
 
--- app/src/main/java/be/ehb/oefeningkamelenrace/util/LoadingHandler.java
+++ app/src/main/java/be/ehb/oefeningkamelenrace/util/LoadingHandler.java
@@ -1,6 +1,21 @@
 package be.ehb.oefeningkamelenrace.util;
 
 import android.os.Handler;
+import android.os.Message;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoadingHandler extends Handler {
+
+    private ProgressBar progressBar;
+    private TextView tv;
+
+    public LoadingHandler(ProgressBar progressBar, TextView tv) {
+        this.progressBar = progressBar;
+        this.tv = tv;
+    }
+
+    public void handleMessage(Message message){
+        int progress = message.arg1;
+    }
 }
